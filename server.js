d6cf7c5ec3168642c766e4e72ab6dafe6f9b5249
Manojkumar144 <manojsantha1442000@gmail.com>
2023-10-25 21:03:38 +0530

    simplified the code

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('My Name: Manoj');
 });
 
 const port = 4000;
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+server.listen(port);
