8b9d75f60b6801e106db3fa09db06d20f9fee7bd
Manojkumar144 <manojsantha1442000@gmail.com>
2023-10-27 21:16:08 +0530

    returning a cutom response

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('My Name: Manoj');
+  const url = req.url;
+
+  res.setHeader('Content-Type', 'text/plain');
+  if (url === '/home') {
+    res.write('Welcome home');
+  } else if (url === '/about') {
+    res.write('Welcome to About Us page');
+  } else if (url === '/node') {
+    res.write('Welcome to my Node.js project');
+  } else {
+    res.statusCode = 404; 
+    res.write('Not Found');
+  }
+  res.end();
 });
 
 const port = 4000;
