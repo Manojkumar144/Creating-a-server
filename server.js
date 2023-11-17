ac5758c603887dd971347e7df3e95be401be440a
Manojkumar144 <manojsantha1442000@gmail.com>
2023-11-17 10:21:01 +0530

    implementing express.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
-const routes = require('./routes');
-const server = http.createServer(routes);
-
-const port = 4000;
-server.listen(port);
+const express = require('express');
+const app = express();
+app.use((req, res, next)=>
+{
+    console.log('in the middleware');
+    next();// allows the request to continue to the next middleware in line
+});
+app.use((req, res, next)=>
+{
+    console.log('in another middleware');
+    res.send('<h1>Hello from Express js</h1>');
+});
+app.listen(4000);
