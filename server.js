51ead23ad65d35bdd6539be84874d0f2b1ba1794
Manojkumar144 <manojsantha1442000@gmail.com>
2023-11-17 12:07:42 +0530

    parsed the incoming request using third part package : body parser.

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req, res, next)=>
-{
-    console.log('in the middleware');
-    next();// allows the request to continue to the next middleware in line
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req, res, next)=>
+{ 
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Product Name"><input type="text" name="size" placeholder="Size"><button type ="submit"> Add Product</button></form>');
 });
-app.use((req, res, next)=>
+
+app.post('/product',(req, res, next)=>
 {
-    console.log('in another middleware');
-    res.send('<h1>Hello from Express js</h1>');
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next)=>
+{
+    res.send('<h1>Hello from Express!</h1>');
 });
 app.listen(4000);
